57bff4809135fed1167664d37eda3fc434955c72
NickLatkovich <nicklatkovich@gmail.com>
2017-10-27 02:24:07 +0300

    Random

--- SimpleUtils/Rand.cs
+++ SimpleUtils/Rand.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleUtils {
-    public static class SimpleUtils {
+    public static class Rand {
+
+        private static Random _rand = new Random( );
+
+        public static bool BRand( ) => _rand.Next(2) == 0;
+
     }
 }
