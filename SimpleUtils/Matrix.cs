2bdae543c1c641541ba9e3581cb3aa6f4fd7fdaa
NickLatkovich <nicklatkovich@gmail.com>
2017-10-26 21:49:40 +0300

    Init

---
+++ SimpleUtils/Matrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtils {
+    public class Matrix<T> {
+
+        private uint _width;
+        private uint _height;
+        private T[ , ] _arr;
+
+        public uint Width {
+            get => _width;
+            set => throw new NotImplementedException( );
+        }
+
+        public uint Height {
+            get => _height;
+            set => throw new NotImplementedException( );
+        }
+
+        public UPoint Size => new UPoint(_width, _height);
+
+        public Matrix(uint width, uint height) {
+            _width = width;
+            _height = height;
+            _arr = new T[width, height];
+        }
+
+        public Matrix(uint width, uint height, T defaultValue) : this(width, height) {
+            Clear(defaultValue);
+        }
+
+        public void Clear(T clearValue) {
+            for (uint i = 0u; i < _width; i++) {
+                for (uint j = 0u; j < _height; j++) {
+                    _arr[i, j] = clearValue;
+                }
+            }
+        }
+
+        public T this[uint i, uint j] {
+            get => _arr[i, j];
+            set {
+                _arr[i, j] = value;
+            }
+        }
+
+        public T this[UPoint point] {
+            get => this[point.X, point.Y];
+            set => this[point.X, point.Y] = value;
+        }
+    }
+}
---
+++ SimpleUtils/UPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtils {
+    public struct UPoint {
+
+        public uint X;
+        public uint Y;
+
+        public UPoint(uint x, uint y) {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj is UPoint == false) {
+                return false;
+            }
+            return this == (UPoint)obj;
+        }
+        public override int GetHashCode( ) => (int)(X * 27644437u ^ Y);
+
+        public static bool operator ==(UPoint self, UPoint other) => self.X == other.X && self.Y == other.Y;
+        public static bool operator !=(UPoint self, UPoint other) => !(self == other);
+    }
+}
